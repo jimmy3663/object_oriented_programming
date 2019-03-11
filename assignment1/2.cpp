53880c01a2bbbf7620f4d80cec4700ccbf02ad9e
jimmy3663 <jimmy3663@naver.com>
2019-03-11 18:22:57 +0900

    problem2

--- assignment1/2.cpp
+++ assignment1/2.cpp
@@ -3,5 +3,40 @@
 using namespace std;
 
 int main(){
-	
+	int init = 20000;
+	int f = 720;
+	int af= 80;
+	int td;
+	int tmp;
+	int cf;
+
+	cout<< "Initial Balance : 20000"<<endl;
+	while(1){
+		cout<< "Entering each travel distance : ";
+		cin>> td;
+
+		if(td==-1){
+			cout<<"Program Exit"<<endl;
+			break;
+		}
+		if(td<41){
+			cout<<"Fare : "<< f<<endl;
+			cf=init-f;
+			init =cf; 
+			cout<<"Current balance: "<<cf<<endl;
+		}
+		else {
+			tmp = (td-40)/8;
+			tmp = tmp*af;
+			cout<<"Fare : "<<tmp<<endl;
+			tmp = tmp + f;
+			cf= init-tmp;
+			init=cf;
+			cout<<"Current balance: " <<cf<<endl;
+		}
+		
+
+	}
+
+
 }
