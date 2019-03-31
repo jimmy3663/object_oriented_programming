2f3b0f83569a2e6311375c4c4373aafae54910db
jimmy3663 <jimmy3663@naver.com>
2019-03-31 23:51:00 +0900

    asterisk with comment

--- assignment1/1-2.cpp
+++ assignment1/1-2.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-void asterisk(int num){
+void asterisk(int num){ // printing diamond asterisk function 
 	int a;
 	a=num/2;
 
-	for(int i=1; i<=a+1; i++){
+	for(int i=1; i<=a+1; i++){ // from the top to middle row 
 		for(int j=i; j<a+1; j++)
 			cout<<" ";
 		for(int k=1; k<(i*2);k++)
@@ -15,7 +15,7 @@ void asterisk(int num){
 		cout<<endl;
 	}
 
-	for(int i=a; i>=1; i--){
+	for(int i=a; i>=1; i--){ // from middle to bottom row 
 		for(int j=a+1;j>i;j--)
 			cout<<" ";
 		for(int k=1; k<(i*2);k++)
@@ -32,13 +32,13 @@ int main(){
 	int num;
 
 	cout<<"input odd number between 5~29 : ";
-	cin>>num;
+	cin>>num; //num is input number between 5 ~ 29 
 	
-	if(num<5)
+	if(num<5) // exception case 
 		cout<<"input should be bigger than 4"<<endl;
-	else if(num%2==0)
+	else if(num%2==0) // exception case 
 		cout<<"input should be odd number"<<endl;
-	else
+	else 
 		asterisk(num);
 
 }
