a30fbd21dba5837cc47a8012443d127fd3b15338
jimmy3663 <jimmy3663@naver.com>
2019-04-01 00:28:51 +0900

    4.cpp with comment

--- assignment1/4.cpp
+++ assignment1/4.cpp
@@ -5,35 +5,24 @@ using namespace std;
 int main(){
 	int a, b, c;
 
-	int max=0;
-	int min=0;
+	int max=0; // maximum 
+	int min=0; // minimum 
 
 	cout<<"input a, b, c: ";
 	cin>>a;
 	cin>>b;
 	cin>>c;
 
-	/*float answer;
-
-	b= b*b;
-	float tmp;
-	tmp = b/(4*a);
-	answer= c-tmp;
-	if(a>0)
-		cout<<"minimum is : "<<answer<<endl;
-	else 
-		cout<<"maximum is : "<<answer<<endl;*/
-
 	for(int i=-30; i<=30; i++){
 		int tmp = a*i*i + b*i + c;
-		if(max<tmp)
-			max=tmp;
+		if(max<tmp) // if tmp is bigger than max 
+			max=tmp; // max is tmp
 	}
 
 	for(int i=-30; i<=30; i++){
-		int tmp= a*i*i + b*i + c;
-		if(min>tmp)
-			min=tmp;
+		int tmp= a*a*i + b*i + c;
+		if(min>tmp) // if minimum is smaller than tmp 
+			min=tmp; // them minimum is tmp 
 	}
 
 	cout<<"max : "<<max<<endl;
