b7d5ef1eaf014999a88ecbe66aafe9a54d734a18
jimmy3663 <jimmy3663@naver.com>
2019-04-01 01:27:10 +0900

    problem 6 with comment

--- assignment1/6.cpp
+++ assignment1/6.cpp
@@ -7,32 +7,35 @@ int main(){
 	int num;
 	cin>>num;
 	
-	int **arr=new int*[num];
-	int *ans=new int[num];
+	int **arr=new int*[num]; // double array 
+	int *ans=new int[num]; 
 
 	for(int i=0; i<num; i++){
-		arr[i] = new int [6];
+		arr[i] = new int [6]; // making array 
 	}
 
 	for(int i=0; i<num; i++){
 		for(int j=0; j<6; j++){
-			cin>>arr[i][j];
+			cin>>arr[i][j]; // putting value to array 
 		}
 	}
 
 	for(int i=0; i<num; i++){
-			int a;
-			a=pow(arr[i][0]-arr[i][3],2);
+			int a; 
+			a=pow(arr[i][0]-arr[i][3],2); // pow is square root 
 			int b;
 			b=pow(arr[i][1]-arr[i][4],2);
 			double c;
-			c= sqrt(a+b);
+			c= sqrt(a+b); // sqrt is square 
 			
-			if(c==arr[i][2]+arr[i][5])
+			if(c==arr[i][2]+arr[i][5]) // if c is equal to arr[i][2]+arr[i][5] 
 				cout<<"1"<<endl;
-			else if(c>arr[i][2]+arr[i][5])
+			else if(c>arr[i][2]+arr[i][5])// if c is bigger than arr[i][2]+arr[i][5] 
 				cout<<"0"<<endl;
-			else 
+			else if(arr[i][0]==arr[i][3] && arr[i][1] == arr[i][4] && arr[i][2]==arr[i][5])
+				// if all element is same 
+				cout<<"-1"<<endl;
+			else // if c is smaaller than arr[i][2]+arr[i][5] 
 				cout<<"2"<<endl;
 	}
 
