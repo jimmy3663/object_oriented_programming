789f95355771a24cab4cc0905df41707af79aec0
jimmy3663 <jimmy3663@naver.com>
2019-04-05 21:01:44 +0900

    correction

--- assignment1/1-1.cpp
+++ assignment1/1-1.cpp
@@ -8,13 +8,13 @@ void asterisk(int num){ // printing star asterisk function
 	a=num/2; 
 
 	for(int i= 0; i<a; i++){ // i is coloumn 
-		for(int j=0; j<num+2; j++){ // j is row 
+		for(int j=0; j<num; j++){ // j is row 
 			if(i==0){
 				if(j==0) // in first row condition 
 					cout<<"$";
-				else if(j==a+1)
+				else if(j==a)
 					cout<<"$";
-				else if(j==num+1)
+				else if(j==num-1)
 					cout<<"$";
 				else 
 					cout<<" ";
@@ -22,9 +22,9 @@ void asterisk(int num){ // printing star asterisk function
 			else{
 				if(i==j) // between first row to middle row condition 
 					cout<<"$";
-				else if(j==a+1)
+				else if(j==a)
 					cout<<"$";
-				else if(j==num+1-i)
+				else if(j==num-1-i)
 					cout<<"$";
 				else 
 					cout<< " ";
@@ -33,36 +33,36 @@ void asterisk(int num){ // printing star asterisk function
 		cout<<endl; 
 	}
 
-	for(int i=0; i<num+2; i++){ // middle row condition 
-		if(i==a+1)
+	for(int i=0; i<num; i++){ // middle row condition 
+		if(i==a)
 			cout<<"$";
 		else 
 			cout<<" ";
 	}
 	cout<<endl;
 
-	a=num/2;
-	int b;
-	b=a-1;
+	//a=num/2;
+	//int b;
+	//b=a-1;
 
 	for(int i=0; i<a; i++){ // after the middle row condition 
-		for(int j=0; j<num+2; j++){
+		for(int j=0; j<num; j++){
 			if(i==a-1){
-				if(j==0) // between row after middle to last row condition
+				if(j==0) // last row condition
 					cout<<"$";
-				else if(j==a+1)
+				else if(j==a)
 					cout<<"$";
-				else if(j==num+1)
+				else if(j==num-1)
 					cout<<"$";
 				else 
 					cout<<" ";
 			}
 			else{
-				if(j==b-i) //last row condition 
+				if(j==a-i-1) //from middle to bottom row  
 					cout<<"$";
-				else if(j==a+1)
+				else if(j==a)
 					cout<<"$";
-				else if(j==a+3+i)
+				else if(j==a+i+1)
 					cout<<"$";
 				else 
 					cout<<" ";
